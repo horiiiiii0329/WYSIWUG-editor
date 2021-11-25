e10693c228e89c0bd08a5bfe6396da932596ce4e
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-25 23:31:07 +0900

    solve error get in by add didmount

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import { Editor, EditorState } from "draft-js";
 import "draft-js/dist/Draft.css";
 
 const Home: NextPage = () => {
+  const [mount, setMount] = useState(false);
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  return (
+    <>
+      {mount && <Editor editorState={editorState} onChange={setEditorState} />}
+    </>
+  );
 };
 
 export default Home;
