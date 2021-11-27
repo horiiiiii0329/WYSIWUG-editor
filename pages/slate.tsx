ab2327c39c9e3b978d4cac8d783b358022461432
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-27 23:50:15 +0900

    render blocvk code

--- pages/slate.tsx
+++ pages/slate.tsx
@@ -1,12 +1,13 @@
 // Import React dependencies.
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useCallback } from "react";
 // Import the Slate editor factory.
-import { createEditor } from "slate";
+import { createEditor, Editor, Transforms } from "slate";
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
 
 import { BaseEditor, Descendant } from "slate";
 import { ReactEditor } from "slate-react";
+
 type CustomElement = { type: "paragraph"; children: CustomText[] };
 type CustomText = { text: string };
 declare module "slate" {
@@ -17,20 +18,60 @@ declare module "slate" {
   }
 }
 
+const initialValue: CustomElement[] = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
 function SlateComponent() {
-  const initialValue: CustomElement[] = [
-    {
-      type: "paragraph",
-      children: [{ text: "A line of text in a paragraph." }],
-    },
-  ];
   const [value, setValue] = useState<Descendant[]>(initialValue);
+
   const editor = useMemo(() => withReact(createEditor()), []);
+
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   return (
     <Slate value={value} onChange={setValue} editor={editor}>
-      <Editable />
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (event.key === "`" && event.ctrlKey) {
+            // Prevent the "`" from being inserted by default.
+            event.preventDefault();
+            const [match] = Editor.nodes(editor, {
+              match: (n) => n.type === "code",
+            });
+            // Otherwise, set the currently selected blocks type to "code".
+            Transforms.setNodes(
+              editor,
+              { type: match ? "paragraph" : "code" },
+              { match: (n) => Editor.isBlock(editor, n) }
+            );
+          }
+        }}
+      />
     </Slate>
   );
 }
 
+const CodeElement = (props: any) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props: any) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
 export default SlateComponent;
