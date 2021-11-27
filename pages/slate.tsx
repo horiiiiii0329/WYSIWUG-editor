ec94e4968d7e9a9563f7add4f4292d40bbe20073
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-27 23:12:20 +0900

    done walkthrough

--- pages/slate.tsx
+++ pages/slate.tsx
@@ -5,8 +5,32 @@ import { createEditor } from "slate";
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
 
-function slate() {
-  return null;
+import { BaseEditor, Descendant } from "slate";
+import { ReactEditor } from "slate-react";
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomText = { text: string };
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
 }
 
-export default slate;
+function SlateComponent() {
+  const initialValue: CustomElement[] = [
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ];
+  const [value, setValue] = useState<Descendant[]>(initialValue);
+  const editor = useMemo(() => withReact(createEditor()), []);
+  return (
+    <Slate value={value} onChange={setValue} editor={editor}>
+      <Editable />
+    </Slate>
+  );
+}
+
+export default SlateComponent;
