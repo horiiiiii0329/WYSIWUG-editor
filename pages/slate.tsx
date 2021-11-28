d0f030ff6baab4c31a1dfffb6ee95bf1d72bad1c
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-28 13:52:19 +0900

    added button

--- pages/slate.tsx
+++ pages/slate.tsx
@@ -25,6 +25,38 @@ const initialValue: CustomElement[] = [
   },
 ];
 
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.bold === true,
+      universal: true,
+    });
+    return !!match;
+  },
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === "code",
+    });
+    return !!match;
+  },
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : "code" },
+      { match: (n) => Editor.isBlock(editor, n) }
+    );
+  },
+};
+
 function SlateComponent() {
   const [value, setValue] = useState<Descendant[]>(initialValue);
 
@@ -45,6 +77,24 @@ function SlateComponent() {
 
   return (
     <Slate value={value} onChange={setValue} editor={editor}>
+      <div>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleBoldMark(editor);
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleCodeBlock(editor);
+          }}
+        >
+          Code Block
+        </button>
+      </div>
       <Editable
         renderElement={renderElement}
         renderLeaf={renderLeaf}
@@ -52,30 +102,16 @@ function SlateComponent() {
           if (!event.ctrlKey) {
             return;
           }
+          // Replace the `onKeyDown` logic with our new commands.
           switch (event.key) {
-            // When "`" is pressed, keep our existing code block logic.
             case "`": {
               event.preventDefault();
-              const [match] = Editor.nodes(editor, {
-                match: (n) => n.type === "code",
-              });
-              Transforms.setNodes(
-                editor,
-                { type: match ? "paragraph" : "code" },
-                { match: (n) => Editor.isBlock(editor, n) }
-              );
+              CustomEditor.toggleCodeBlock(editor);
               break;
             }
-            // When "B" is pressed, bold the text in the selection.
             case "b": {
               event.preventDefault();
-              Transforms.setNodes(
-                editor,
-                { bold: true },
-                // Apply it to text nodes, and split the text node up if the
-                // selection is overlapping only part of it.
-                { match: (n) => Text.isText(n), split: true }
-              );
+              CustomEditor.toggleBoldMark(editor);
               break;
             }
           }
