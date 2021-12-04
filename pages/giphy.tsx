a7214aa69c59e6ff51cfec41e0a65235d547d510
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-12-04 18:25:15 +0900

    griphyn components

--- pages/giphy.tsx
+++ pages/giphy.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 import axios from "axios";
 import { useState, useEffect } from "react";
 import styles from "../styles/giphy.module.css";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 function Giphy({ handleSelected }: any) {
   const [gifs, setGif] = useState<any>([]);
   const [term, setTerm] = useState("");
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(20);
   const [input_ref, setInput] = useState<any>(null);
 
   const onSearchSubmit = (e: any) => {
@@ -61,8 +63,20 @@ function Giphy({ handleSelected }: any) {
           onKeyDown={onSearchSubmit}
         />
       </div>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <Masonry columnsCount={3} gutter="10px">
+          {gifs.map((item: any, index: number) => (
+            // eslint-disable-next-line @next/next/no-img-element
 
-      <img src={gifs[0].images.fixed_width_downsampled.url} alt="aaaaaaaaa" />
+            <img
+              src={item.images.fixed_width_downsampled.url}
+              alt="aaaaaaaaa"
+              className={styles.image}
+              key={index}
+            />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
     </div>
   );
 }
