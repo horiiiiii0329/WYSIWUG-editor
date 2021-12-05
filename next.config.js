c4cf2dfb6b59ab5c80a8e26038a409ea3adbc609
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-12-05 13:43:31 +0900

    yarn add draft.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  module: {
+    loaders: [
+      {
+        test: /plugin\.css$/,
+        loaders: ["style-loader", "css"],
+      },
+    ],
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,99 +1,49 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { useState, useEffect, useCallback } from "react";
+import React, { useEffect, useRef } from "react";
+import Editor, { createEditorStateWithText } from "@draft-js-plugins/editor";
+import editorStyles from "./editorStyles.module.css";
+import { useState } from "react";
+import { EditorState } from "draft-js";
+import createSideToolbarPlugin from "@draft-js-plugins/side-toolbar";
+import "@draft-js-plugins/side-toolbar/lib/plugin.css";
 import styles from "../styles/Home.module.css";
 
-import "draft-js/dist/Draft.css";
-import { getVisibleSelectionRect } from "draft-js";
-import Inserter from "../components/Inserter";
-import {
-  RichUtils,
-  DraftEditorCommand,
-  EditorState,
-  DraftHandleValue,
-  Editor,
-} from "draft-js";
+const sideToolbarPlugin = createSideToolbarPlugin();
+const { SideToolbar } = sideToolbarPlugin;
+const plugins = [sideToolbarPlugin];
 
-const Home: NextPage = () => {
-  const [mount, setMount] = useState(false);
+function Aaa() {
   const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
+    createEditorStateWithText("")
   );
-  const [style, setStyle] = useState({ display: "none" });
-
-  const handleKeyCommand = useCallback(
-    (
-      editorCommand: DraftEditorCommand,
-      editorState: EditorState
-    ): DraftHandleValue => {
-      const newState = RichUtils.handleKeyCommand(editorState, editorCommand);
-      if (newState) {
-        setEditorState(newState);
-        return "handled";
-      }
-      return "not-handled";
-    },
-    []
-  );
-  const shouldShowInlineStyleInserter = (editorState: EditorState) => {
-    // 現在の選択範囲を取得する
-    const selection = editorState.getSelection();
-    // 選択範囲である SelectionState の anchor と focus の位置が同じでない
-    if (selection.isCollapsed()) {
-      return false;
-    }
-    // 現在の記事の内容を取得する
-    const content = editorState.getCurrentContent();
-    // 選択中の場所に Block 要素を含んでいない
-    // 正確には始点と終点の内部もチェックする必要があるが、始点と終点 atomic ではないという簡易的な判定で代用している
-    return (
-      content.getBlockForKey(selection.getAnchorKey()).getType() !== "atomic" &&
-      content.getBlockForKey(selection.getFocusKey()).getType() !== "atomic"
-    );
-  };
-
-  const getInlineStylePosition = (editorState: EditorState) => {
-    const rect = getVisibleSelectionRect(window);
-    if (!rect) return null;
-    // 必要に応じて調整する
-    return { top: rect.top - 30, left: rect.left };
-  };
-
-  const onChange = useCallback((editorState: EditorState) => {
-    if (shouldShowInlineStyleInserter(editorState)) {
-      const rect = getInlineStylePosition(editorState);
-      if (rect) {
-        setStyle({ display: "block", ...rect });
-      }
-    } else {
-      setStyle({ display: "none" });
-    }
-    setEditorState(editorState);
-  }, []);
 
   useEffect(() => {
-    setMount(true);
+    // fixing issue with SSR https://github.com/facebook/draft-js/issues/2332#issuecomment-761573306
+    setEditorState(createEditorStateWithText("aaaa"));
   }, []);
 
+  const editor = useRef<Editor | null>(null);
+
+  const onChange = (value: EditorState): void => {
+    setEditorState(value);
+  };
+
+  const focus = (): void => {
+    editor.current?.focus();
+  };
+
   return (
-    <>
-      <div className={styles.wrapper}>
-        <Inserter
-          style={style}
-          onClickBoldButton={() => handleKeyCommand("bold", editorState)}
-        />
-
-        {mount && (
-          <Editor
-            editorState={editorState}
-            onChange={onChange}
-            handleKeyCommand={handleKeyCommand}
-          />
-        )}
-      </div>
-    </>
+    <div className={styles.editor} onClick={focus}>
+      <Editor
+        editorState={editorState}
+        onChange={onChange}
+        plugins={[sideToolbarPlugin]}
+        ref={(element) => {
+          editor.current = element;
+        }}
+      />
+      <SideToolbar />
+    </div>
   );
-};
+}
 
-export default Home;
+export default Aaa;
